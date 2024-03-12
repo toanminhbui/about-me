412b01b69e453eadf8d09a082599a6e519296898
toanminhbui <toanbui@umich.edu>
2024-03-12 02:58:05 -0400

    added about me

--- js/script.js
+++ js/script.js
@@ -39,26 +39,25 @@ let rotateText = () => {
 };
 
 rotateText();
+setInterval(rotateText, 3000);
 // Wait for the DOM content to load
 document.addEventListener('DOMContentLoaded', function() {
-  // Get the About Me section element
-  var aboutMeSection = document.getElementById('about-me');
+    // Get the About Me section element
+    var aboutMeSection = document.getElementById('about-me');
 
-  // Hide the About Me section initially
-  aboutMeSection.style.display = 'none';
+    // Hide the About Me section initially
+    aboutMeSection.style.display = 'none';
 
-  // Add a scroll event listener to the window
-  window.addEventListener('scroll', function() {
-      // Check if the user has scrolled past a certain point
-      if (window.scrollY > 200) { // Adjust this value according to your needs
-          // Show the About Me section
-          aboutMeSection.style.display = 'block';
-      } else {
-          // Hide the About Me section if the user scrolls back up
-          aboutMeSection.style.display = 'none';
-      }
-  });
+    // Add a scroll event listener to the window
+    window.addEventListener('scroll', function() {
+        // Check if the user has scrolled past a certain point
+        if (window.scrollY > 400) { // Adjust this value according to your needs
+            // Show the About Me section
+            aboutMeSection.style.display = 'block';
+            aboutMeSection.classList.add('fade-in');
+        } else {
+            // Hide the About Me section if the user scrolls back up
+            aboutMeSection.style.display = 'none';
+        }
+    });
 });
-
-setInterval(rotateText, 3000);
-
